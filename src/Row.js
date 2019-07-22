5f6a6b24718549cfbc2e3cfc70e42474b38f62d0
Vivek Sainanee <viveksainanee@gmail.com>
2019-07-22 14:27:59 -0400

    added lightweight responsive

--- src/Row.js
+++ src/Row.js
@@ -9,6 +9,28 @@ import img05 from './assets/05.png';
 import img06 from './assets/06.png';
 
 class Row extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  // Event listener to update screen size in state
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  // Removes screen size updater
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  // Updates screen size in state
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
   render() {
     // this is an array of gem images
     let imageFiles = [img00, img01, img02, img03, img04, img05, img06];
@@ -18,8 +40,8 @@ class Row extends Component {
       <img
         src={imageFiles[id]}
         alt="gem"
-        height="60px"
-        width="60px"
+        height={this.state.width < 500 ? '40px' : '60px'}
+        width={this.state.width < 500 ? '40px' : '60px'}
         key={uuid()}
       />
     ));
